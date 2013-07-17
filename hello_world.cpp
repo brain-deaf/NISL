003345a22f68abd5ffc50b9c6574c55ee79da06e
unknown <patrickoneil.music@gmail.com>
2013-07-17 13:48:37 -0700

    added hello_world example

--- hello_world.cpp
+++ hello_world.cpp
@@ -2,8 +2,7 @@
 
 int main(int argc, char *argv[]){
   Py_Initialize();
-  PyRun_SimpleString("from hello_world import say_hello\n"
-                     "say_hello()\n");
+  PyRun_SimpleString("from hello_world import say_hello\nsay_hello()");
   Py_Finalize();
   return 0;
 }
